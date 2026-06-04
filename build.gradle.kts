e1c3bfbaca49a30294968351b7febe0f33b098b8
Ethan Dsouza <100838354+ethan-dsouza-dev@users.noreply.github.com>
2026-06-03 22:26:34 -0400

    Add google api key mapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("tools.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
