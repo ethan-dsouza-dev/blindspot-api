35dc512484e4b488c9321450889112606100f4a6
Kxngbron <bbanks4@my.centennialcollege.ca>
2026-08-14 16:17:59 -0400

    Update FavoriteRepository.kt

--- src/main/kotlin/com/blindspot/blindspotapi/backend/favorites/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/blindspot/blindspotapi/backend/favorites/repository/FavoriteRepository.kt
@@ -2,6 +2,7 @@ package com.blindspot.blindspotapi.backend.favorites.repository
 
 import com.blindspot.blindspotapi.backend.favorites.entity.FavoriteEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 interface FavoriteRepository : JpaRepository<FavoriteEntity, UUID> {
@@ -10,5 +11,6 @@ interface FavoriteRepository : JpaRepository<FavoriteEntity, UUID> {
 
     fun existsByUserIdAndPlaceId(userId: UUID, placeId: String): Boolean
 
+    @Transactional
     fun deleteByUserIdAndPlaceId(userId: UUID, placeId: String)
 }
